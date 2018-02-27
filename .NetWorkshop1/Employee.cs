050298e7e44614707a95fea9a493aae148a3fb6f
MHabib96 <33306932+MHabib96@users.noreply.github.com>
2018-02-27 16:46:46 +0000

    Add files via upload

---
+++ .NetWorkshop1/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApplication
+{
+    class Employee : IComparable
+    {
+        //Declaring Employee variables
+        public String name;
+        public String family;
+        public int idNumber = 0;
+
+        //Employee Constructor
+        public Employee(String name, String family, int idNumber)
+        {
+            this.name = name;
+            this.family = family;
+            this.idNumber = idNumber;
+        }
+
+        //Compares instance with specified object
+        //Returns int specifying if instance procedes, follows, or in same position.
+        int IComparable.CompareTo(object obj)
+        {
+            int returnValue;
+            Employee temp = (Employee)obj;
+            if (this.idNumber > temp.idNumber)
+                returnValue = 1;
+            else
+                if (this.idNumber < temp.idNumber)
+                returnValue = -1;
+            else
+                returnValue = -0;
+            return returnValue;
+        }
+
+    }
+}
---
+++ .NetWorkshop1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create empty ArrayList.
+            ArrayList empl1 = new ArrayList();
+
+            //Populating ArrayList with 3 Employee objects.
+            empl1.Add(new Employee("David", "Livingstone", 25));
+            empl1.Add(new Employee("Tim", "Ellis", 17));
+            empl1.Add(new Employee("Richard", "Jones", 8));
+
+            //Loops through ArrayList, printing out contained object names.
+            Console.WriteLine("List of Employee: ");
+            foreach(Employee emp in empl1)
+                Console.WriteLine(emp.name);
+           
+            //Sorts ArrayList using .sort() method
+            //Loops through ArrayList, printing out newly sorted data.
+            empl1.Sort();
+            Console.WriteLine("List of Employee After Sorting: ");
+            foreach (Employee emp in empl1)
+                Console.WriteLine(emp.name);
+
+            Console.ReadLine(); //Keep cmd tab open when running application
+
+        }
+    }
+}
