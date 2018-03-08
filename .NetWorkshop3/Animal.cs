af0a554e9dd7e7fd8fc8c254ba0399922fa2e3ac
MHabib96 <33306932+MHabib96@users.noreply.github.com>
2018-03-08 18:58:10 +0000

    Add files via upload

---
+++ .NetWorkshop3/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workshop3
+{
+    class Animal
+    {
+        //Auto property for getter/setter
+        public int ID { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ .NetWorkshop3/Herbivorous.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workshop3
+{
+    class Herbivorous
+    {
+        public int ID { get; set; }
+        
+        public string AnimalName { get; set; }
+
+        public string Food
+        {
+            get
+            {
+                return this.AnimalName + " eats grass and grains!!";
+            }
+        }
+    }
+}
