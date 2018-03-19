4ac8f3f2e63c08245a5b1f0533cea992b8c4e332
MHabib96 <33306932+MHabib96@users.noreply.github.com>
2018-03-18 20:41:51 -0700

    Add files via upload

---
+++ .NetWorkshop5/Models/SportsDB.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetWorkshop5.Models
+{
+    public class SportsDB
+    {
+        static List<Product> _Products;
+
+        static SportsDB()
+        {
+            _Products = new List<Product>();
+            _Products.Add(new Product
+            {
+                ProductID = 1,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Category = "Watersports",
+                Price = 275M
+            });
+        }
+
+        public IList<Product> Products
+        {
+            get { return _Products; }
+        }
+
+    }
+}
